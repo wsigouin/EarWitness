ca9ce249b6929dadc1bf5c80ca8115097749f404
wsigouin <willsigouin@gmail.com>
2017-03-27 11:15:39 -0400

    Initial Front End

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function showValueMin(newValue)
+{
+	document.getElementById("confidenceMinOut").innerHTML=newValue+"%";
+}
+function showValueWeek(newValue)
+{
+	document.getElementById("confidenceWeekOut").innerHTML=newValue+"%";
+}
+function showValueTwo(newValue)
+{
+	document.getElementById("confidenceTwoOut").innerHTML=newValue+"%";
+}
+function showValueChoice(newValue)
+{
+	document.getElementById("choiceOut").innerHTML=newValue+"%";
+}
